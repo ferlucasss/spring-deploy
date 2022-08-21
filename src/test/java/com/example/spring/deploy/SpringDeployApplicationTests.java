4cd20815d54d7f060908a9e9978214564d95b4cb
Lucas Fernandez <lucasfernandezepe@gmail.com>
2022-08-21 18:31:43 -0300

    Initial commit

---
+++ src/test/java/com/example/spring/deploy/SpringDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
